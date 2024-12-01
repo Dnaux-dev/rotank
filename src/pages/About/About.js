88eb3130c9bce6243bd52d086d92f62c55415c36
emperooid <awosikaemmanueldefirst@gmail.com>
2024-12-01 02:27:29 +0100

    new update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import './About.css'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="hero-section">
+    <div className="hero-overlay">
+      <h2>Rottank operates a modern fleet of around 50 unique vessels</h2>
+      <div className="info-box">
+        <h3>
+        VT is an independent company with committed employees. Many have been living a maritime existence from generation to generation.</h3>
+        <p>
+          Rottank has an ultramodern fleet of more than 30 unique ships.
+          Varying from 210 MT (MTS Velden) to 13500 MT (MTS Vorstenbosch | Largest inland
+          tanker in the World)
+        </p>
+      </div>
+    </div>
+    <div className='core-values'>
+    <hr className='color-bar'></hr>
+    </div>
+    <div className='core-values'>
+    <hr className='color-bar'></hr>
+    </div>
+    <div className='core-values'>
+    <hr className='color-bar'></hr>
+    </div>
+    <div className='core-values'>
+    <hr className='color-bar'></hr>
+    </div>
+
+   
+  </div>
   )
 }
 
